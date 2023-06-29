b5e93ddcc34a54aa2d59dc935d164d7ac88515ab
Leonid <leonidfromouterspace@gmail.com>
2023-06-29 13:18:45 +0300

    initial commit

---
+++ b.js
@@ -0,0 +1,12 @@
+const egorovJeka = {
+    years: 25,
+}
+egorovJeka.country = 'Belarus'
+egorovJeka.city = 'Minsk'
+egorovJeka.name = 'Jendos'
+egorovJeka.secondName = 'Egorov' 
+egorovJeka.weight = 85
+egorovJeka.height = 180
+egorovJeka.fate = 'rastizdos'
+
+console.log(egorovJeka)
