381543aeb5575adf043f6d45eb7b7d9200e5fc41
EgorovEu <82610540+EgorovBel@users.noreply.github.com>
2023-06-29 13:22:46 +0300

    Update b.js

--- b.js
+++ b.js
@@ -1,12 +1,12 @@
 const egorovJeka = {
-    years: 25,
+    years: 26,
 }
 egorovJeka.country = 'Belarus'
 egorovJeka.city = 'Minsk'
 egorovJeka.name = 'Jendos'
 egorovJeka.secondName = 'Egorov' 
 egorovJeka.weight = 85
-egorovJeka.height = 180
-egorovJeka.fate = 'rastizdos'
+egorovJeka.height = 185
+egorovJeka.fate = 'chiller'
 
 console.log(egorovJeka)
