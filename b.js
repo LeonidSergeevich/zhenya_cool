ed15bbcb292fd1669adf59ffca10c89549cd1da4
Leonid <leonidfromouterspace@gmail.com>
2023-06-29 14:23:44 +0300

    уляля

--- b.js
+++ b.js
@@ -1,12 +1,34 @@
-const egorovJeka = {
-    years: 26,
+class Man {
+    constructor(country, city, name, secondName, weight, height, fate) {
+        this.country = country;
+        this.city = city;
+        this.name = name;
+        this.secondName = secondName;
+        this.weight = weight;
+        this.height = height;
+        this.fate = fate;
+    }
 }
-egorovJeka.country = 'Belarus'
-egorovJeka.city = 'Minsk'
-egorovJeka.name = 'Jendos'
-egorovJeka.secondName = 'Egorov' 
-egorovJeka.weight = 85
-egorovJeka.height = 185
-egorovJeka.fate = 'chiller'
 
-console.log(egorovJeka)
+class Show {
+    static createUl(Obj) {
+        let ul = document.createElement("ul");
+        for (let key in Obj) {
+            if (Obj.hasOwnProperty(key)) {
+                ul.appendChild(Show.createLi(key + ' : ' + Obj[key]));
+            }
+        }
+        document.body.appendChild(ul);
+    }
+    static createLi(content) {
+        let li = document.createElement("li");
+        li.innerHTML = content
+        return li;
+    }
+}
+
+let ManObj = new Man('Minsk', 'Minsk', 'Jendos', 'Egorov', 85, 185, 'chiller');
+document.addEventListener("DOMContentLoaded", function () {
+    alert('Hello!')
+    Show.createUl(ManObj);
+});
